48756ab7f3f02dc996e05ce0d7e76b5e8f0cbfd3
Israel-Eskape <isra.ixoye@gmail.com>
2021-07-20 16:38:40 -0500

    Se agrega la opción si se desea guardar como documento formatos .docx

---
+++ README.md
@@ -0,0 +1,11 @@
+# Scraping LaCuerda.Net
+
+Busca las canciones a partir del nombre de la banda y de la canción
+y las muestra en pantalla. Después de visualizarlo también da la opción si 
+desea guardarlo en un documento .docx
+
+# Requisitos
+BeautifulSoup
+requests
+pandas
+Document
--- scrapingLacuerdaNet.py
+++ scrapingLacuerdaNet.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd 
+from docx import Document
 
-print("Nombre de la banda : ")
+print("Name of band : ")
 band = input()
-print("Nombre de la canción : ")
+print("Name of song : ")
 song = input()
 
 band = band.replace(' ','_')
@@ -15,10 +16,31 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 le = soup.find_all('div', id= 't_body')
+name = soup.find_all('div', id = 'tH1')
 
+names = list()
 letra = list()
+for j in name:
+    names.append(j.text)
+
 for i in le:
-    letra.append(i.text)
-    
-letra  = "".join(letra)
-print(letra)
+   letra.append(i.text)
+
+if(len(letra) != 0):
+    names = "".join(names)
+    letra  = "".join(letra)
+    print(names)
+    print(letra)
+
+    print("Save as document ? Y/N")
+    resp = input()
+    if(resp == 'Y' or resp == 'y' or resp == 'S' or resp == 's'):
+        print(' saving ... ')
+        document = Document()
+        document.add_heading(names,0)
+        document.add_paragraph(letra)
+
+        document. save(song+'.docx')
+        print(song+'.docx \n saved ... ')
+else:
+    print('No se encontró la canción intente de nuevo')
